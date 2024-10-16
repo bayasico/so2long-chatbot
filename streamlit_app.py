f8dd4ec59066adc56455290e599cb3d44bd45b81
bayasico <kentkoba98@gmail.com>
2024-10-16 14:38:59 +0900

    tmp: for masato

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,9 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 卒論用Chatbot p1, p2")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "デモなので一般利用はできません"
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
